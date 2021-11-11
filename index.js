261225257f70e3a142cfe7bf2edfca510ca8e7f0
Md Firoz Mia <67627130+Firoz01@users.noreply.github.com>
2021-11-12 02:44:52 +0600

    set user route

--- index.js
+++ index.js
@@ -29,6 +29,8 @@ async function run() {
     const database = client.db("watchOfSwitzerland");
     const productsCollection = database.collection("products");
     const ordersCollection = database.collection("orders");
+    const reviewsCollection = database.collection("reviews");
+    const usersCollection = database.collection("users");
 
     app.get('/products', async (req, res) => {
       const products = await productsCollection.find().toArray();
@@ -49,12 +51,66 @@ async function run() {
       res.json(orders);
     })
 
+    app.get('/orders/:email', async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const order = await ordersCollection.findOne(query).toArray();
+      res.json(order);
+    })
+
+
+    app.get('/reviews', async (req, res) => {
+      const reviews = await reviewsCollection.find().toArray();
+      res.json(reviews);
+    })
+
+     app.get("/user/:email", async (req, res) => {
+       const email = req.params.email;
+       const query = { email: email };
+       const user = await usersCollection.findOne(query);
+       let isAdmin = false;
+       if (user?.role === "admin") {
+         isAdmin = true;
+       }
+       res.json({ admin: isAdmin });
+     });
+
+    app.post('/user', async (req, res) => {
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
+      res.json(result);
+    })
+
+     app.put("/user", async (req, res) => {
+       const user = req.body;
+       const filter = { email: user.email };
+       const options = { upsert: true };
+       const updateDoc = {
+         $set: user,
+       };
+       const result = await usersCollection.updateOne(
+         filter,
+         updateDoc,
+         options
+       );
+       res.json(result);
+     });
+
     app.post('/order', async (req, res) => {
       const order = req.body;
+      Object.assign(order, { status: "pending" });
       console.log(order);
       const result = await ordersCollection.insertOne(order);
       res.json(result);
     })
+
+    
+    app.post('/review', async (req, res) => {
+       const review = req.body;
+      const result = await reviewsCollection.insertOne(review);
+      res.json(result);
+    });
+
 
     }
     finally {
